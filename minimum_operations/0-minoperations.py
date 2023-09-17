e6e85db4585b27a4b40168333c5d03a98ff8a331
jubrilabdulazeez <abdulazeez_jubril@DESKTOP-L5KKGJQ>
2023-09-17 22:43:01 +0200

    new commit

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""generates the number of operations possible for a given variabe"""
+
+
+def minOperations(n):
+    if n == 1:
+        return 0
+
+    operations = 0
+    factors = []
+    i = 2
+
+    # Find the prime factors of n
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+
+    if n > 1:
+        factors.append(n)
+
+    # Calculate the sum of prime factors
+    for factor in factors:
+        operations += factor
+
+    return operations
